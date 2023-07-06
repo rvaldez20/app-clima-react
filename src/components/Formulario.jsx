5de9db2665d877cf5f05e9b91a796ac3b0adf2b7
rvaldez20 <rvaldez20@gmail.com>
2023-07-06 13:01:31 -0600

    [ADD] Se crea formulario: para obtener la ciudad y el país

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,9 +2,39 @@
 
 const Formulario = () => {
    return (
-      <form>
-         <h1>Formulario</h1>
-      </form>
+      <div className="contenedor">
+         <form>
+            <div className="campo">
+               <label htmlFor="ciudad">Ciudad</label>
+               <input 
+                  type="text" 
+                  id="ciudad"
+                  name="ciudad"
+               />
+            </div>
+            <div className="campo">
+               <label htmlFor="pais">País</label>
+               <select 
+                  name="pais" 
+                  id="pais"
+               >
+                  <option value="">-- Seleccione un País --</option>
+                  <option value="US">Estados Unidos</option>
+                  <option value="MX">México</option>
+                  <option value="AR">Argentina</option>
+                  <option value="CO">Colombia</option>
+                  <option value="CR">Costa Rica</option>
+                  <option value="ES">España</option>
+                  <option value="PE">Perú</option>
+               </select>
+            </div>
+
+            <input 
+               type="submit"
+               value='Consultar Clima'
+            />
+         </form>
+      </div>
    )
 }
 
