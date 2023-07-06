d51c263e1ebc2722801d358352977488dc7a9958
rvaldez20 <rvaldez20@gmail.com>
2023-07-06 12:52:37 -0600

    [ADD] Se crea la estructura de directorios y algunos componentes
    => Componete: AppClima.jsx
    => Componete: Formulario.jsx
    => Directorios: components | context | hooks
    => se cargan styeles y fonts

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,12 @@
+import Formulario from "./Formulario"
+
+
+const AppClima = () => {
+   return (
+      <main className="dos-columnas">
+         <Formulario />
+      </main>
+   )
+}
+
+export default AppClima
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+
+
+const Formulario = () => {
+   return (
+      <form>
+         <h1>Formulario</h1>
+      </form>
+   )
+}
+
+export default Formulario
