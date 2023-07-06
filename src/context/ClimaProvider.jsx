75890dd64a72013fd88d97f76a3786980dcaa857
rvaldez20 <rvaldez20@gmail.com>
2023-07-06 14:09:02 -0600

    [ADD] se usan las funciones del context para obtener los datos del formulario

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -3,13 +3,32 @@ import { useState, createContext } from 'react'
 const ClimaContext = createContext()
 
 const ClimaProvider = ({ children }) => {
-   // logic provider
-   
+   //!----- logic provider ----
+
+   //* States
+   const [busqueda, setBusqueda] = useState({
+     ciudad: '',
+     pais: '', 
+   })
+
+   //* Functions
+   const datosBusqueda = e => {
+      setBusqueda({
+         ...busqueda,
+         [e.target.name]: e.target.value
+      })
+   }
+
+   const consultarClima = datos => {
+      console.log(datos)
+   }
 
    return (
       <ClimaContext.Provider
          value={{
-            
+            busqueda,
+            datosBusqueda,
+            consultarClima,
          }}
       >
          { children }
