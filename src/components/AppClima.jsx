a4b40b95e1128129bdd5da675415b6bad046530c
rvaldez20 <rvaldez20@gmail.com>
2023-07-06 15:35:55 -0600

    [ADD] se muestran los resultados del clima dela ciudad seleccionada

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -1,10 +1,18 @@
-import Formulario from "./Formulario"
+import useClima from '../hooks/useClima'
+import Formulario from './Formulario'
+import Resultado from './Resultado'
 
 
 const AppClima = () => {
+
+   const {resultado} = useClima()
+
+
    return (
       <main className="dos-columnas">
          <Formulario />
+
+         {resultado?.name && <Resultado />}         
       </main>
    )
 }
